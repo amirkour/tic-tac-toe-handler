c8c7c7c4f4a9adb923b17fbee5cc8a88e3ab1787
Amir Kouretchian <amir.kouretchian@gmail.com>
2022-09-14 21:41:11 -0700

    some config updates to pull min-max-tic-tac-toe in correctly

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # tic-tac-toe-handler
 aws lambda handler for tic-tac-toe implementation
+
+# linking min-max-tic-tac-toe
+
+while developing, you can't get min-max-tic-tac-toe imports to resolve until you link that project:
+
+`npm link ../min-max-tic-tac-toe`
+
+make sure you build the min-max-tic-tac-toe project before linking to it
+
+# layer updating
+
+this handler depends on min-max-tic-tac-toe - to bundle that project and upload it to an aws lambda layer, and then associate that layer to this handler, see notes in [min-max-tic-tac-toe's readme](https://github.com/amirkour/min-max-tic-tac-toe)
+
+# uploading/updating in aws
+
+first, make sure the min-max-tic-tac-toe code/layer is updated (read above.)
+
+then, simply run:
+
+`npm run redeploy`
+
+might be a good idea to just try a build first: `npm run build` or `npm run rebuild` and make sure things compile!
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
+import { Game, strategies } from "min-max-tic-tac-toe";
+const { RandomNextMoveGetter } = strategies;
 
-export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-    console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'hello world from ts land and make sure its working!?',
-        }),
-    };
+export const handler = async (
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+  const game = new Game({ nmg: new RandomNextMoveGetter({ min: 0, max: 8 }) });
+  const winner = game.getWinner();
+  const board = game.getBoard();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "hello world from ts land and make sure its working!?",
+      winner,
+      board,
+    }),
+  };
 };
