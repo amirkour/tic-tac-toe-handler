1900f447e76558adee67baf2c9f1c9bab9049072
Amir Kouretchian <akadmin@Amirs-Personal-MacBook-Pro.local>
2022-12-13 14:20:02 -0800

    updates to lambda handler that apply the computers move to the requesters tic tac toe game

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,41 @@ interface ResponseBody {
   nextToMove: MOVE;
 }
 
+/*
+ * Helper that'll check if the given game has a winner.  If it does:
+ * will set the given response's outcome (w/ a description of the
+ * game's result) and return true.  Returns false otherwise.
+ */
+const gameOver = (game: Game, response: ResponseBody): boolean => {
+  const winner = game.getWinner();
+  if (winner == null) return false;
+
+  response.outcome =
+    winner === DRAW ? `Game over - it's a tie!` : `Game over - ${winner} wins!`;
+  return true;
+};
+
+/*
+ * Main/handler for this AWS Lambda function.
+ * At a high-level, this handler will:
+ *
+ * - expect game data for a tic-tac-toe game in the request body
+ * - parse said game data and apply the caller's move
+ * - make a move for the computer
+ * - return the resulting game board and outcome, along w/
+ *   an http 200 status code
+ *
+ * If there are any errors along the way, an appropriate http status
+ * code and error message will result.
+ */
 export const handler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-  let statusCode: number = 200;
+
+  let statusCode = 200;
   const response: ResponseBody = {
     error: null,
     board: [],
@@ -25,39 +53,39 @@ export const handler = async (
 
   try {
     if (event.body == null) {
-      const game = new Game({
-        nmg: new RandomNextMoveGetter({ min: 0, max: 8 }),
-      });
-
-      response.board = game.getBoard();
-      response.nextToMove = game.whosTurn();
-    } else {
-      const requestBody: any = JSON.parse(event.body);
-      if (!requestBody.board || !(requestBody.board instanceof Array)) {
-        statusCode = 400;
-        throw 'Request must include a "board" parameter, which must be an array of string';
-      }
-
-      if (!Number.isInteger(requestBody.move)) {
-        statusCode = 400;
-        throw 'Request must include a "move" parameter, which must be an intenger';
-      }
-
-      const move = requestBody.move;
-      const board = requestBody.board;
-      const game = new Game({ nmg: new RandomNextMoveGetter(), board });
-      game.makeMove(move);
-
-      const winner = game.getWinner();
-      if (winner != null) {
-        response.outcome =
-          winner === DRAW
-            ? `Game over - it's a tie!`
-            : `Game over - ${winner} wins!`;
-      }
-      response.nextToMove = game.whosTurn();
-      response.board = game.getBoard();
+      statusCode = 400;
+      throw "Request must include an event body";
     }
+
+    const requestBody: any = JSON.parse(event.body);
+    if (!requestBody.board || !(requestBody.board instanceof Array)) {
+      statusCode = 400;
+      throw 'Request must include a "board" parameter, which must be an array of strings';
+    }
+
+    if (!Number.isInteger(requestBody.move)) {
+      statusCode = 400;
+      throw 'Request must include a "move" parameter, which must be an intenger';
+    }
+
+    const move = requestBody.move;
+    const board = requestBody.board;
+    const game = new Game({
+      nmg: new RandomNextMoveGetter({ min: 0, max: 8 }),
+      board,
+    });
+
+    do {
+      game.makeMove(move);
+      if (gameOver(game, response)) break;
+
+      const computersMove = game.getNextMove();
+      if (computersMove != null) game.makeMove(computersMove);
+      if (gameOver(game, response)) break;
+    } while (false);
+
+    response.nextToMove = game.whosTurn();
+    response.board = game.getBoard();
   } catch (e) {
     statusCode = statusCode === 200 ? 500 : statusCode;
     response.error = `${e}`;
